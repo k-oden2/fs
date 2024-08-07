7c566f4d95ae8a5c7ad765cdf3c06047afd06f6f
k-oden2 <164519984+k-oden2@users.noreply.github.com>
2024-08-08 00:38:15 +0700

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -2,6 +2,7 @@
 
 #ffmpeg_split
 wget https://raw.githubusercontent.com/k-oden2/fs/main/f.sh
-chmod +x f.sh
+wget https://raw.githubusercontent.com/k-oden2/fs/main/f4.sh
+chmod +x f.sh f4.sh
 
 echo -e "<-- DONE -->"
